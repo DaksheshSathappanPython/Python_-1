0c9cad496dd5575eee27e05d7319a97ee1b7e0cc
PythonFirebolt <104211195+PythonFirebolt@users.noreply.github.com>
2022-04-22 19:22:32 +0530

    Add files via upload

---
+++ type_conversion_casting.py
@@ -0,0 +1,60 @@
+# a=5
+# print(float(a)) # 5.0
+# print(str(a)) # 5
+
+# b=6.7
+# print(int(b)) # 6
+# print(str(b)) # 6.7
+
+# x=3 ; y=6.2 ; z=4 ; q=5.3
+# print(x+z) # 7
+# print(y+q) # 11.5
+# print(x+y) # 9.2
+# print(q+z) # 9.3
+
+# s='hello'
+# print(int(s)) # ValueError
+# print(float(s)) # ValueError
+
+# w='3'
+# print(int(w)) # 3
+# print(float(w)) # 3.0
+
+# r='5.6'
+# print(int(r)) # ValueError
+# print(float(r)) # 5.6
+# d=float(r)
+# print(int(d)) # 5
+
+# x=4 ; m='py' ; v=3.2
+# print(type(x)) # int
+# print(type(m)) # str
+# print(type(v)) # float
+# print(x+m) # TypeError
+# print(m+v) # TypeError
+
+
+# a='python'  ;  b='programming'
+# print(a+b)  # pythonprogramming
+# print(a,b) # python programming
+# print(a+''+b) # pythonprogramming
+# print(a+' '+b) # python programming
+# print(a+'    '+b) # python      programming
+# print(a+'-'+b) # python-programming
+# print(a+'@'+b) # python@programming
+# print(a+' - '+b) # python - programming
+# print(a+' -> '+b) # python -> programming
+
+
+# a=1 ; b=3.4 ; c='py'
+# print(a+b) # 4.4
+# print(a,b) # 1 3.4
+# print(a,c) # 1 py
+# print(b,c) # 3.4 py
+# print(c,c) # py py
+# print(a,a) # 1 1
+
+
+# a=3 ; b='3.10.4'
+# print('we are learning python'+str(a)+' '+'version used is',b)
+# print('we are learning python'+str(a),'version used is',b)
